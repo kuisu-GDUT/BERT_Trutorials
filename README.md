fcb89eb464d165a758b23fcfb9d2e66be85deb14
kuisu <kuisu_dgut@163.com>
2023-11-12 12:03:25 +0800

    补充与NLP相关的介绍

---
+++ README.md
@@ -0,0 +1,94 @@
+## torch tutorial
+torchtext主要分为3部分: Field，Dataset，Iteration
+### Field
+```python
+def tokenizer(x): return jieba.lcut(x)
+sentence_field = Field(
+    sequential=True,
+    tokenize=tokenizer,
+    lower=False,
+    batch_first=True,
+    include_lengths=True
+)
+label_field = Field(sequential=False, use_vocab=False)
+```
+Field类可以用张量Tensor表示常见的文本处理数据。 其包含一个Vocab词表对象，该对象对Field中的文本数据及其向量化表示进行定义。
+- squential: 数据是否为序列数据，默认为True。如果为False，则不能使用分词  
+- use_vocab: 是否使用字典，默认为True。如果为False， 那么输入的数据类型必须是树枝类型。  
+- init_token: 文本的起始字符，默认为None  
+- eos_token: 文本的结束字符，默认为None  
+- tokenizer: 用于进行分词的具体方法，默认为Python标准的字符串对象分割函数split()。也可以进行特殊指定，如 `tokenize="spacy"`
+- stop_words: 预处理过程中丢弃的词
+- preprocessing/postprocessing: 指名该Field对象数据在数值化前，后进行的流水线处理。默认均为None
+- build_vocab(): 通过训练数据构建词表，并通过 max_size指定其中词频最大的茜1000个词纳入考虑。  
+这里vectors=Glove()表示预训练好的词向量进行初始化。注意这里仅仅是将预训练的词向量进行初始化，
+并未模型中实际使用该预训练词向量，如果不使用下面的语句将 glove.6b.300d 等预训练词向量倒入，
+则词向量通常会由网络中Embedding层负责训练。
+```python
+TEXT.build_vocab(train,
+                 vectors = GloVe(name = '6B',dim = 300),
+                 max_size = 10000,
+                 min_freq = 10)
+
+LABEL.build_vocab(train)
+```
+
+```python
+model.embedding.weight.data = TEXT.vocab.vectors
+model.embedding.weight.requires_grad = False
+```
+### Dataset
+这里我们需要做的一般是继承 `torchtext.data.Dataset`， 然后重写自己的Dataset。写Dataset时，
+最主要的是Example和Field的结合
+```python
+from torchtext.data import Dataset, Example
+
+class SentenceDataset(Dataset):
+    def __int__(self, data_path, sentence_field, label_field):
+        fields = [("sentence",sentence_field),("label",label_field)]
+        examples = []
+        with open(data_path, "r") as f_json:
+            file_content = json.load(f_json)
+            self.sentence_list = []
+            self.label_list = []
+            for data in file_content:
+                self.sentence_list.append(data['sentence'])
+                self.label_list.append(data['label'])
+                
+        for index in range(len(self.sentence_list)):
+            examples.append(Example.fromlist([self.sentence_list[index], self.label_list[index]], fields))
+
+        super().__init__(examples, fields)
+        
+    @staticmethod
+    def sort_key(input):
+        return len(input.sentence)
+```
+这里将输入的文本，标签和Field进行绑定，也就是告诉Field它具体处理哪些东西，然后使用 `super().__init__(examples,fields)`
+来调用父类的初始化。这里还有一个 `def sort_key(input)`，这个方法帮助后面的iteration进行数据排序用的关键词，
+其实在Iteration中可以直接设置用于排序的关键词，但是因为在Field里面使用类 `include_lengths` 关键词，
+导致后面的iteration直接指定关键词无法进行正常的排序。  
+对于上面的example和Field进行绑定时，这里使用的训练数据是由标签的，所以标签位置就直接写，若数据无标签:
+```python
+examples.append(Example.fromlist([self.sentence_list[index],None],fields))
+```
+
+### Iterator
+```python
+train_iterator = BucketIterator(
+    train_dataset,
+    batch_size=batch_size,
+    device="cuda",
+    sort_within_batch=True,
+    shuffle=True
+)
+test_iterator = Iterator(
+    test_dataset,
+    batch_size=batch_size,
+    device="cuda",
+    sort=False,
+    shuffle=True
+)
+```
+这里使用了 `BucketIterator, Iterator`, 因为BucketIterator可以自动选择长度类似的文本组成一个batch，
+所以用于训练数据，而测试数据一般不需要该操作。
--- seq2seq.py
+++ seq2seq.py
@@ -1,20 +1,19 @@
 import logging
+import os
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
-
 import spacy
 import numpy as np
-
 import random
 import math
 import time
-
 from tqdm import tqdm
+
+from models.seq2seq import Seq2Seq, Encoder, Decoder
 
 logging.basicConfig(level=logging.INFO, filename="seq2seq.log")
 
@@ -27,149 +26,16 @@ torch.cuda.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 
-class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
-
-        self.hid_dim = hid_dim
-        self.n_layers = n_layers
-
-        self.embedding = nn.Embedding(input_dim, emb_dim)
-
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, src):
-        # src = [src len, batch size]
-
-        embedded = self.dropout(self.embedding(src))
-
-        # embedded = [src len, batch size, emb dim]
-
-        outputs, (hidden, cell) = self.rnn(embedded)
-
-        # outputs = [src len, batch size, hid dim * n directions]
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # outputs are always from the top hidden layer
-
-        return hidden, cell
-
-
-class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
-
-        self.output_dim = output_dim
-        self.hid_dim = hid_dim
-        self.n_layers = n_layers
-
-        self.embedding = nn.Embedding(output_dim, emb_dim)
-
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
-
-        self.fc_out = nn.Linear(hid_dim, output_dim)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, input, hidden, cell):
-        # input = [batch size]
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # n directions in the decoder will both always be 1, therefore:
-        # hidden = [n layers, batch size, hid dim]
-        # context = [n layers, batch size, hid dim]
-
-        input = input.unsqueeze(0)
-
-        # input = [1, batch size]
-
-        embedded = self.dropout(self.embedding(input))
-
-        # embedded = [1, batch size, emb dim]
-
-        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
-
-        # output = [seq len, batch size, hid dim * n directions]
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # seq len and n directions will always be 1 in the decoder, therefore:
-        # output = [1, batch size, hid dim]
-        # hidden = [n layers, batch size, hid dim]
-        # cell = [n layers, batch size, hid dim]
-
-        prediction = self.fc_out(output.squeeze(0))
-
-        # prediction = [batch size, output dim]
-
-        return prediction, hidden, cell
-
-
-class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder, device):
-        super().__init__()
-
-        self.encoder = encoder
-        self.decoder = decoder
-        self.device = device
-
-        assert encoder.hid_dim == decoder.hid_dim, \
-            "Hidden dimensions of encoder and decoder must be equal!"
-        assert encoder.n_layers == decoder.n_layers, \
-            "Encoder and decoder must have equal number of layers!"
-
-    def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        # src = [src len, batch size]
-        # trg = [trg len, batch size]
-        # teacher_forcing_ratio is probability to use teacher forcing
-        # e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
-
-        batch_size = trg.shape[1]
-        trg_len = trg.shape[0]
-        trg_vocab_size = self.decoder.output_dim
-
-        # tensor to store decoder outputs
-        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
-
-        # last hidden state of the encoder is used as the initial hidden state of the decoder
-        hidden, cell = self.encoder(src)
-
-        # first input to the decoder is the <sos> tokens
-        input = trg[0, :]
-
-        for t in range(1, trg_len):
-            # insert input token embedding, previous hidden and previous cell states
-            # receive output tensor (predictions) and new hidden and cell states
-            output, hidden, cell = self.decoder(input, hidden, cell)
-
-            # place predictions in a tensor holding predictions for each token
-            outputs[t] = output
-
-            # decide if we are going to use teacher forcing or not
-            teacher_force = random.random() < teacher_forcing_ratio
-
-            # get the highest predicted token from our predictions
-            top1 = output.argmax(1)
-
-            # if teacher forcing, use actual next token as next input
-            # if not, use predicted token
-            input = trg[t] if teacher_force else top1
-
-        return outputs
-
-
 class Seq2SeqRun():
     def __int__(self):
         pass
 
-    def load_data(self):
+    def load_data(self, data_root="/Users/kuisu/Documents/Python/colab-notebooks/pytorch-seq2seq"):
         # load data
         spacy_de = spacy.load('de_core_news_sm')
         spacy_en = spacy.load('en_core_web_sm')
+        logging.info("|load data|de_core_news_sm and en_core_web_sm load finished!")
+        assert os.path.exists(data_root), f"{data_root} not exits"
 
         def tokenize_de(text):
             """
@@ -195,8 +61,9 @@ class Seq2SeqRun():
         train_data, valid_data, test_data = Multi30k.splits(
             exts=('.de', '.en'),
             fields=(SRC, TRG),
-            root="/Users/kuisu/Documents/Python/colab-notebooks/pytorch-seq2seq"
+            root=data_root
         )
+        logging.info(f"|load data\n|train src:{train_data[0].src}\n|train trg:{valid_data[0].trg}")
         logging.info(f"Number of training examples: {len(train_data.examples)}")
         logging.info(f"Number of validation examples: {len(valid_data.examples)}")
         logging.info(f"Number of testing examples: {len(test_data.examples)}")
@@ -313,6 +180,7 @@ class Seq2SeqRun():
         elapsed_mins = int(elapsed_time / 60)
         elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
         return elapsed_mins, elapsed_secs
+
     def run(self):
         # main
         device = torch.device("mps")
@@ -327,7 +195,7 @@ class Seq2SeqRun():
         optimizer = optim.Adam(model.parameters())
         TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
 
-        criterion = nn.CrossEntropyLoss(ignore_index=TRG_PAD_IDX)
+        criterion = nn.CrossEntropyLoss(ignore_index=TRG_PAD_IDX) # 将pad的损失设置为0
 
         N_EPOCHS = 2
         CLIP = 1
@@ -357,7 +225,6 @@ class Seq2SeqRun():
         test_loss = self.evaluate(model, test_iterator, criterion)
 
         logging.info(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
-
 
 
 if __name__ == '__main__':
